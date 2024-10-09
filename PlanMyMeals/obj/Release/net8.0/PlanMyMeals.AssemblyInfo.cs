898ffbce96cf7ef9e9711bfda8137ce9e8712c5a
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-10-09 15:25:51 -0500

    Vertical Diet Updates

--- PlanMyMeals/obj/Release/net8.0/PlanMyMeals.AssemblyInfo.cs
+++ PlanMyMeals/obj/Release/net8.0/PlanMyMeals.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanMyMeals")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483ccf6e58fb4cfa2cc1cb4c8d73f1f4d8a5f408")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanMyMeals")]
