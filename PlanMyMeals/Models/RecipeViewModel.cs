3151e965bc49449b4fdaa399d75334725eb249d9
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-10-13 14:07:06 -0500

    RecipeViewModel

--- PlanMyMeals/Models/RecipeViewModel.cs
+++ PlanMyMeals/Models/RecipeViewModel.cs
@@ -7,7 +7,22 @@ public class RecipeViewModel
     public List<MealIngredient> mealsIngredients { get; set; }
 
 
+
+
+
+
     //functions
+    public List<MealIngredient> addIngToMeal(Meal m, List<MealIngredient> mList, Ingredient mealIng)
+    {
+
+        MealIngredient newIng = new MealIngredient();
+        newIng.MealId = m.MealId;
+        newIng.IngredientId = mealIng.IngredientId;
+
+        mList.Add(newIng);
+        Console.WriteLine(newIng);
+        return mList;
+    }
 
 
 }
